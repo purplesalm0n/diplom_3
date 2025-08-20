61208044f3bfb076a525a999834c3897a8230e64
Polikanov Vadim <lastresortforcake@gmail.com>
2025-08-20 10:31:23 +0300

    parameterized constructor

--- src/test/java/ru/praktikum/stellarburgers/ui/tests/ConstructorNavigationTests.java
+++ src/test/java/ru/praktikum/stellarburgers/ui/tests/ConstructorNavigationTests.java
@@ -2,9 +2,7 @@ package ru.praktikum.stellarburgers.ui.tests;
 
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
-import io.qameta.allure.junit4.AllureJunit4;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import ru.praktikum.stellarburgers.ui.config.WebBaseTest;
 import ru.praktikum.stellarburgers.ui.pages.ConstructorPage;
 import ru.praktikum.stellarburgers.ui.pages.MainPage;
@@ -16,18 +14,34 @@ import static org.hamcrest.Matchers.equalTo;
 public class ConstructorNavigationTests extends WebBaseTest {
 
     @Test
-    @Story("Переходы по вкладкам: Соусы / Начинки / Булки")
-    public void tabs_switch_ok() {
+    @Story("Клик по 'Булки' – показывает раздел 'Булки'")
+    public void openBuns_showsBuns() {
+        new MainPage(driver); // просто открыта главная
+        ConstructorPage constructor = new ConstructorPage(driver);
+
+        constructor.openSauces();
+        constructor.openBuns();
+
+        assertThat(constructor.currentTitle(), equalTo("Булки"));
+    }
+
+    @Test
+    @Story("Клик по 'Соусы' – показывает раздел 'Соусы'")
+    public void openSauces_showsSauces() {
         new MainPage(driver);
         ConstructorPage constructor = new ConstructorPage(driver);
 
         constructor.openSauces();
         assertThat(constructor.currentTitle(), equalTo("Соусы"));
+    }
+
+    @Test
+    @Story("Клик по 'Начинки' – показывает раздел 'Начинки'")
+    public void openFillings_showsFillings() {
+        new MainPage(driver);
+        ConstructorPage constructor = new ConstructorPage(driver);
 
         constructor.openFillings();
         assertThat(constructor.currentTitle(), equalTo("Начинки"));
-
-        constructor.openBuns();
-        assertThat(constructor.currentTitle(), equalTo("Булки"));
     }
 }
